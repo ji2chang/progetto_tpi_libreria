0ab5f204a1f8d5cc16fa3e7c318e4fadfafda65a
ji2chang <wrz20070109@outlook.com>
2026-01-21 19:24:21 +0100

    Updated app.py: implemented all routes

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,28 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_cors import CORS
+from fake_db import FakeDatabase
+
+localDatabase = FakeDatabase()
 app = Flask(__name__)
 CORS(app)
 
 @app.get("/api/libri")
 def list_books():
-    ...
+    return jsonify(localDatabase.all())
 
 @app.post("/api/libri")
 def add_book():
-    ...
+    localDatabase.add()
+    return jsonify({"success": True})
 
 @app.delete("/api/libri/<int:id>")
 def delete_book_by_id(id: int):
-    ...
+    success = localDatabase.delete(id)
+    return jsonify({"success": success})
 
 @app.delete("/api/libri")
 def delete_all_books():
-    ...
-
+    return jsonify({"success": True})
 
 if __name__ == "main":
     app.run(host="0.0.0.0",port=12345,debug=True)
